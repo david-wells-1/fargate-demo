c2fa321f89993e202a5f23d32f21e20bb63be66d
David Wells <david-wells-1@users.noreply.github.com>
2021-06-08 16:38:02 +0100

    Update alb.tf

--- alb.tf
+++ alb.tf
@@ -38,8 +38,3 @@ resource "aws_lb_listener" "ecs" {
     type             = "forward"
   }
 }
-
-output "aws_lb" {
-  value       = "http://${aws_lb.ecs.dns_name}"
-  description = "The public dns name of the external lb"
-}
