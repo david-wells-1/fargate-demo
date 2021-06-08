2f43635e4619b0fcfdaf478c0f316ea4842e459b
David Wells <david-wells-1@users.noreply.github.com>
2021-06-08 16:14:03 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">=0.13.0"
 
   backend "s3" {
-    bucket  = "641621341658-tfstate"
+    bucket  = "ACCOUNT_ID_REMOVED-tfstate"
     encrypt = "true"
     key     = "ecs-nginx-demo.tfstate"
     region  = "eu-west-2"
